89c4e2d4f7d318353685700bf6d3522df9e77631
Helder Alves de Medeiros <helderdek@gmail.com>
2017-12-14 14:59:19 -0200

    dando aquele estilo maroto

--- src/index.js
+++ src/index.js
@@ -19,7 +19,12 @@ class App extends Component {
             selectedVideo: null
         };
 
-        YTSearch({key: API_KEY, term:'pokemon'}, (data) => {
+        this.videoSearch('skate');
+        
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (data) => {
             this.setState({ videos: data,
             selectedVideo: data[0]
             });
@@ -29,7 +34,7 @@ class App extends Component {
     render(){
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
                 <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList
                  onVideoSelect={selectedVideo => this.setState({selectedVideo})}
